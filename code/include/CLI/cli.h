a985586675ce676f4881eddb51d3f55c11e766e7
Moussi <ines-tigh@hotmail.fr>
2019-04-11 16:30:28 +0200

    Mettre à jour la structure du code

---
+++ code/include/CLI/cli.h
@@ -0,0 +1,4 @@
+#ifndef __CLI__H__
+#define __CLI__H__
+
+#endif
---
+++ code/include/defaultValues.h
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifndef __DEFAULTVALUES__H__
+#define __DEFAULTVALUES__H__
+
+/* --------------- Définitions des colors ---------------- */
+#define vert {0, 7, 227}
+
+#endif
