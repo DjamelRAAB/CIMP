82f6c45affafe79bf2bdf77d5efb9e5b0d152413
Moussi <ines-tigh@hotmail.fr>
2019-04-25 15:54:10 +0200

    modification windows

--- code/src/windows/windows.c
+++ code/src/windows/windows.c
@@ -24,3 +24,13 @@ windows *initWindows(const char path[]){
     return w; 
 }
 
+int fermerWindows( windows *w){
+    if(w == NULL)
+	return 0;
+    if(w->renderer != NULL && w->fenetre != NULL){
+        SDL_DestroyRenderer(w->renderer);
+        SDL_DestroyWindows(w->fenetre);
+        return 1;
+    }
+    return 0;
+}
